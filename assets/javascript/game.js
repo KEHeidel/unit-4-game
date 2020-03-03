6298bf0688b6cd5cdb9fd5d997b9a8e7502e25de
Kortnie Heidel <kortnie.evans@gmail.com>
2020-03-03 15:58:33 -0500

    adds variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Worked on this with my study group: Amy Haerr, John Pendergrass, Haley McLemore, and Brooke Guarienti
+
+$(document).ready(function() {
+  // Set variables
+  var targetRandom = 0;
+  var counter = 0;
+  var wins = 0;
+  var losses = 0;
+
+  // Creates crystal object
+  var Crystals = {
+    crystal1: {
+      value: 0
+    },
+    crystal2: {
+      value: 0
+    },
+    crystal3: {
+      value: 0
+    },
+    crystal4: {
+      value: 0
+    }
+  };
